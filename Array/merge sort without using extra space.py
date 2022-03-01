9bb5cb341c3bab185fdbe7c561e32d76e711268f
shivamdustin29 <75965561+shivamdustin29@users.noreply.github.com>
2022-03-01 18:43:04 +0530

    Create merge sort without using extra space.py

---
+++ Array/merge sort without using extra space.py
@@ -0,0 +1,11 @@
+class Solution:
+    def merge(self, arr1, arr2, n, m): 
+        i=n-1
+        j=0
+        while(i>=0 and j<m):
+            if(arr1[i]>=arr2[j]):
+                arr1[i],arr2[j]=arr2[j],arr1[i];
+            i=i-1
+            j=j+1
+        arr1.sort()
+        arr2.sort()
