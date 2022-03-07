c4673d64f974f65e54e5b127a51a0520cff5fe96
shivamdustin29 <75965561+shivamdustin29@users.noreply.github.com>
2022-03-07 18:49:39 +0530

    Create Union of two array.py

---
+++ Array/Union of two array.py
@@ -0,0 +1,28 @@
+if __name__=='__main__':
+    t=int(input())
+    for _ in range(t):class Solution:    
+    #Function to return the count of number of elements in union of two arrays.
+    def doUnion(self,a,n,b,m):
+        a.extend(b)
+        a=set(a)
+        l=len(a)
+        return l
+        
+            
+        
+        #code here
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+nput().strip().split()]
+        b=[int(x) for x in input().strip().split()]
+        ob=Solution()
+        
+        print(ob.doUnion(a,n,b,m))
+#contributed by RavinderSinghPB
+        
+        n,m=[int(x) for x in input().strip().split()]
+        
+        a=[int(x) for x in i
+# } Driver Code Ends
