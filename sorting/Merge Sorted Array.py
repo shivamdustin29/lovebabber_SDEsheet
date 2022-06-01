32098e30198e9c9af9a6408915ccfb5147d4c084
shivamdustin29 <75965561+shivamdustin29@users.noreply.github.com>
2022-06-02 02:51:07 +0530

    Create Merge Sorted Array.py

---
+++ sorting/Merge Sorted Array.py
@@ -0,0 +1,6 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        del(nums1[m:])
+        nums1.extend(nums2)
+        return nums1.sort()
+        
